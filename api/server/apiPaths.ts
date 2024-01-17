542335e45a81cf059c2150e5559ea2ba0d1947d3
FranklinMonro <fbg2468@gmail.com>
2024-01-17 05:16:33 +0000

    currencies

--- api/server/apiPaths.ts
+++
@@ -1,6 +0,0 @@
-import { dirname, resolve } from 'path';
-
-const baseDirectory = dirname(__dirname);
-const swaggerPath = resolve(baseDirectory, 'swagger', 'swaggerRoutes');
-
-export default swaggerPath;
